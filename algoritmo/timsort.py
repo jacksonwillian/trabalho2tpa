2867f93c12ff62a6eb079177c8632d72553cec53
Emanuel Rampinelli <37427276+emanuelrampinelli@users.noreply.github.com>
2019-10-16 20:36:20 -0300

    TimSort - Incompleto mas funcional

---
+++ algoritmo/timsort.py
@@ -0,0 +1,121 @@
+# N = Comprimento so vetor de entrada
+# RUN = Subvetor ordenado que compõe o vetor de entrada
+# Minrun = É o comprimento minimo dos runs.Este numero é calculado baseado no tamanho do vetor.
+RUN = 3
+
+def merge(lado_esq,lado_dirt):
+
+	index_dir 	= 0
+	index_esq 	= 0
+	tam_esq		= len(lado_esq)
+	tam_dir		= len(lado_dirt)
+	new_lst 	= []
+	
+	while index_esq < tam_esq and index_dir < tam_dir:
+			
+		if(lado_esq[index_esq] < lado_dirt[index_dir]):
+			
+			new_lst.append(lado_esq[index_esq])
+			index_esq+=1	
+		
+		else:
+
+			new_lst.append(lado_dirt[index_dir])
+			index_dir+=1
+		
+	new_lst += lado_dirt[index_dir:]
+	new_lst += lado_esq[index_esq:]
+	
+	return new_lst
+
+
+def insertSort(lista):
+
+  for i in range(1,len(lista)):
+    
+    chave = lista[i]
+    j = i-1
+
+    while( j >= 0 and lista[j] > chave):
+    
+    	lista[j+1] = lista[j]
+    	j = j -1
+  
+    lista[j+1] = chave
+   
+  return lista
+
+
+def timSort(lst):
+	
+	sub_lst 	= []
+	lado_esq 	= []
+	lado_dirt 	= []
+	tam 		= len(lst)
+
+	for i in range (0,tam,RUN):
+		
+		lst[i:i+RUN] = insertSort(lst[i:i+RUN])
+	
+	size = RUN
+
+	while size < tam:
+		
+
+		baseESQ = size - RUN
+		endESQ	= size
+
+		baseDIR = size
+		endDIR = size + RUN
+
+		lado_esq = lst[baseESQ:endESQ]
+		lado_dirt = lst[baseDIR:endDIR]
+		
+		print(lado_esq)
+		print(lado_dirt)
+		
+		lst[baseESQ:endDIR] = merge(lado_esq,lado_dirt)
+
+		size = size*2
+			
+	return lst
+	
+
+
+l = [8,1,5,2,1,0]
+print(timSort(l))
+
+
+
+'''
+
+
+def timSort(arr):  
+   	
+	n = len(arr)
+    # Classificar sub-matrizes individuais de tamanho RUN
+	for i in range(0,n,RUN):
+		insertionSort(arr,i,min((i+31),(n-1)))  
+    
+    # comece a mesclar a partir do tamanho RUN (ou 32). Vai mesclar
+    # para formar o tamanho 64, 128, 256 e assim por diante ....
+	size = RUN 
+	while size < n:  
+       
+        # escolhe o ponto inicial da sub-matriz esquerda. Nós
+        # vão mesclar arr [esquerda .. esquerda + tamanho-1]
+        # e arr [esquerda + tamanho, esquerda + 2 * tamanho-1]
+        # Após cada mesclagem, aumentamos a esquerda em 2 *
+        for left in range(0,n,2*size):
+
+            # encontra o ponto final da sub-matriz esquerda
+            # mid + 1 é o ponto inicial da sub-matriz direita
+			mid = left + size - 1
+			right = min((left + 2*size - 1),(n-1))
+    
+           	# mesclar sub-matriz arr [esquerda ..... média] &
+            # arr [médio + 1 .... certo] 
+			merge(arr,left,mid,right)
+          
+		size = 2*size
+'''
