26b3aeaab3bb56d934aba167ef6a7c96025a2297
Emanuel Rampinelli <37427276+emanuelrampinelli@users.noreply.github.com>
2019-10-16 17:29:37 -0300

    MergeSort - liso

---
+++ algoritmo/mergeSort.py
@@ -0,0 +1,45 @@
+def mergeSort(lst):
+
+	if(len(lst) > 1):
+		
+	
+		meio = len(lst)//2;
+
+		lado_esq 	= lst[:meio]
+		lado_dirt 	= lst[meio:]
+
+		lado_esq 	= mergeSort(lado_esq)
+		lado_dirt 	= mergeSort(lado_dirt)	
+
+		return merge(lado_esq,lado_dirt,meio)
+
+	return lst
+
+
+def merge(lado_esq,lado_dirt,meio):
+
+	index_dir 	= 0
+	index_esq 	= 0
+	tam_esq		= len(lado_esq)
+	tam_dir		= len(lado_dirt)
+	new_lst 	= []
+	
+	
+	while index_esq < tam_esq and index_dir < tam_dir:
+		
+		
+		if(lado_esq[index_esq] < lado_dirt[index_dir]):
+			
+			new_lst.append(lado_esq[index_esq])
+			index_esq+=1	
+		
+		else:
+
+			new_lst.append(lado_dirt[index_dir])
+			index_dir+=1
+		
+	
+	new_lst += lado_dirt[index_dir:]
+	new_lst += lado_esq[index_esq:]
+	
+	return new_lst
