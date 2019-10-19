a796f640746d60433eebfa456cd7b9ca9c8f7e30
Jackson Willian <jacksonwilliansilvaagostinho@gmail.com>
2019-10-19 18:48:00 -0300

    heapsort

--- algoritmo/heapsort.py
+++ algoritmo/heapsort.py
@@ -3,43 +3,35 @@ f_comparacao = None
 v_coluna = None
 
 
+
+
+
 # RETORNA O INDICE DO MAIOR FILHO QUE É MAIOR QUE A RAIZ
 # RETORNA -1 QUANDO NÃO ENCONTRA ESSE FILHO 
-def posicaoMaiorFilho(a, i, j):
+def maxheap(a, i, tam):
 	# CALCULA O INDICE DOS FILHOS
-	filhoEsquerdo = ((2*i) + 1) if ((2*i) + 1) <= j else -1
-	filhoDireito = ((2*i) + 2) if ((2*i) + 2) <= j else -1
-
-	# VERIFICA O FILHO MAIOR QUANDO EXISTE DOIS FILHOS
-	if (filhoEsquerdo != -1 and filhoDireito != -1):
-		if f_comparacao( a[filhoEsquerdo][v_coluna], a[filhoDireito][v_coluna] ) == 1 and f_comparacao( a[filhoEsquerdo][v_coluna], a[i][v_coluna] ) == 1:
-			return filhoEsquerdo
-		elif f_comparacao( a[filhoDireito][v_coluna], a[filhoEsquerdo][v_coluna] ) == 1 and f_comparacao( a[filhoDireito][v_coluna], a[i][v_coluna] ) == 1:
-			return filhoDireito
-		else:
-			return -1
-
-	# VERIFICA O FILHO MAIOR QUANDO NÃO EXISTE UM DOS FILHOS
+	maior  = -1
+	
+	if (((2*i) + 1) <= tam) and f_comparacao(a[((2*i) + 1)][v_coluna], a[i][v_coluna] ) == 1: 
+		maior =  ((2*i) + 1)
 	else:
-		if (filhoEsquerdo != -1 ) and f_comparacao( a[filhoEsquerdo][v_coluna], a[i][v_coluna]) == 1: 
-			return filhoEsquerdo
-		elif (filhoDireito != -1 ) and f_comparacao( a[filhoDireito][v_coluna], a[i][v_coluna] ) == 1:
-			return filhoDireito
-		else:
-			return -1
+		maior = i
+		
+	if (((2*i) + 2) <= tam) and f_comparacao(a[((2*i) + 2)][v_coluna], a[maior][v_coluna]) == 1:
+		maior = ((2*i) + 2)
+	
+	if maior != i:
+		a[maior], a[i] = a[i], a[maior]
+		maxheap(a, maior, tam)
+	
+
+def heap(a):
 
 
-# i POSICAO DA RAIZ
-# j ULTIMA POSICAO DO VETOR
-def heap(a, i, j):
-	posicaoMaior = posicaoMaiorFilho(a, i, j)
-	while (posicaoMaior != -1):
-		aux = a[posicaoMaior]
-		a[posicaoMaior] = a[i]
-		a[i] = aux
-		i = posicaoMaior
-		posicaoMaior = posicaoMaiorFilho(a, posicaoMaior, j)
+	printlista(a)
 	return a
+
+
 
 def f_heapsort(a, arg1, arg2):
 
@@ -49,19 +41,65 @@ def f_heapsort(a, arg1, arg2):
 	f_comparacao = arg1
 	v_coluna = arg2
 
-	posicaoInicial = 0
-	posicaoFinal = len(a)-1
-	while ((posicaoFinal -1 ) >= posicaoInicial):
-		posicaoMeio = int(posicaoFinal/2)
-		while( posicaoInicial <= posicaoMeio):
-			a = heap(a, 0, posicaoFinal)
-			posicaoMeio -=  1
-		aux = a[posicaoFinal]
-		a[posicaoFinal] = a[0]
-		a[0] = aux
-		posicaoFinal -= 1
+	j = len(a) - 1
+	tam = len(a) - 1
+	while (j > 0):
+		meio = tam//2
+		while (0 <= meio):
+			maxheap(a, meio, j)
+			meio -= 1
+	
+		a[0], a[j] = a[j], a[0] 
+		j -= 1
 
 	return a
+
+
+# # inicial 232
+# # depois 221
+
+# # RETORNA O INDICE DO MAIOR FILHO QUE É MAIOR QUE A RAIZ
+# # RETORNA -1 QUANDO NÃO ENCONTRA ESSE FILHO 
+# def maxheap(a, i, j):
+#     # CALCULA O INDICE DOS FILHOS
+#     maior  = -1
+	
+#     if (((2*i) + 1) <= j) and (a[((2*i) + 1)] > a[i] ): 
+#         maior =  ((2*i) + 1)
+#     else:
+#         maior = i
+		
+#     if (((2*i) + 2) <= j) and (a[((2*i) + 2)] > a[maior] ):
+#         maior = ((2*i) + 2)
+	
+#     if maior != -1:
+#         a[maior], a[i] = a[i], a[maior]
+#         maxheap(a, maior, j)
+		
+		
+	
+# def f_heapsort(a):
+#     j = len(a) - 1
+#     while (j > 0):
+#         meio = j//2
+#         while (0 <= meio):
+#             maxheap(a, meio, j)
+#             meio -= 1
+#         a[0], a[j] = a[j], a[0] 
+#         j -= 1
+
+#     return a
+
+# #aux = a[j]
+# #a[j] = a[0]
+# #a[0] = aux
+# #j -= 1
+	
+# a = [3, 8, 5, 7, 7, 7, 9, 8, 6, 4, 3, 6, 6, 4, 4, 4, 6, 9]
+
+# f_heapsort(a)
+
+	
 
 
 
