8194d79e4247608ba77a4fd7f448f62b4d8e4723
Jackson Willian <43284155+jacksonwillian@users.noreply.github.com>
2019-10-25 15:45:26 -0300

    Update comparacao.py

--- util/comparacao.py
+++ util/comparacao.py
@@ -6,24 +6,3 @@ def f_compara_texto(a, b):
 		return -1
 	else:
 		return 0
-
-
-# def f_compara_texo(a, b, c):
-# 	if(a[c] > b[c]):
-# 		return 1
-# 	elif(a[c] < b[c]):
-# 		return -1
-# 	else:
-# 		return 0
-
-def f_compara_inteiro(a, b, c):
-	pass
-
-def f_compara_data(a, b, c):
-	pass
-
-def f_comp_gen_C(a,b,c):
-	return (a[c]>b[c])
-
-def f_comp_gen_D(a,b,c):
-	return (a[c]<b[c])
